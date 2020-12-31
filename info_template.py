8b2e343eca2f39ebc68a5ece2951a52b6b84fc36
Noam Landress <noamlandress@microsoft.com>
2020-12-31 17:07:18 +0200

    gitignore

--- info_template.py
+++ info_template.py
@@ -1,4 +1,8 @@
-class system_info:
+import time
+import subprocess
+
+
+class SystemInfo:
     def __init__(self, command, command_output, error="No error received"):
         self.command = command
         self.command_output = command_output
@@ -6,8 +10,53 @@ class system_info:
 
     def __repr__(self):
         delimiter = "\n-----------------------------------\n"
-        return str(delimiter + "command: " + self.command + '\n' + "output: " + self.command_output + '\n' + "error: " + self.error)
+        return str(
+            delimiter + "command: " + self.command + '\n' + "output: " + self.command_output + '\n' + "error: " + self.error + delimiter)
 
 
-noam = system_info("x", "y")
-print(repr(noam))
+def append_content_to_file(command_object, file_path='/tmp/cef_get_info'):
+    output = repr(command_object)
+    command_tokens = ["sudo", "bash", "-c", "printf '" + "\n" + output + "' >> " + file_path]
+    try:
+        write_new_content = subprocess.Popen(command_tokens, stdout=subprocess.PIPE)
+        time.sleep(2)
+        o, e = write_new_content.communicate()
+    except Exception:
+        print(str(command_object.command) + "was not run successfully")
+
+
+def netstat_open_ports():
+    command_to_run = subprocess.Popen(["sudo", "netstat", "-lnpvt"], stdout=subprocess.PIPE)
+    print("Gathering open ports info")
+    o, e = command_to_run.communicate()
+    o = o.decode(encoding='UTF-8')
+    netstat = SystemInfo('netstat -lnpvt', str(o), str(e))
+    append_content_to_file(netstat)
+
+
+def disk_space():
+    command_to_run = subprocess.Popen(["sudo", "df", "-h"], stdout=subprocess.PIPE)
+    print("Gathering free disk space info")
+    o, e = command_to_run.communicate()
+    o = o.decode(encoding='UTF-8')
+    df = SystemInfo('df -h', str(o), str(e))
+    append_content_to_file(df)
+
+
+def memory_space():
+    command_to_run = subprocess.Popen(["sudo", "free", "-m"], stdout=subprocess.PIPE)
+    print("Gathering system memory info")
+    o, e = command_to_run.communicate()
+    o = o.decode(encoding='UTF-8')
+    df = SystemInfo('free -m', str(o), str(e))
+    append_content_to_file(df)
+
+
+def main():
+    netstat_open_ports()
+    disk_space()
+    memory_space()
+
+
+if __name__ == '__main__':
+    main()
