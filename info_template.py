3f7c536fced4c196fceaf8e8d6b19e0487d56279
Noam Landress <noamlandress@microsoft.com>
2021-01-05 10:25:22 +0200

    Fix exceptions

--- info_template.py
+++ info_template.py
@@ -15,27 +15,33 @@ class SystemInfo:
 
 
 command_dict = {
-    "netstat": ["sudo", "netstat", "-lnpvt"],
-    "df": ["sudo", "df", "-h"],
-    "free": ["sudo", "free", "-m"],
-    "iptables": ["sudo", "iptables", "-vnL"]
-
+    "netstat": "sudo netstat -lnpvt",
+    "df": "sudo df -h",
+    "free": "sudo free -m",
+    "iptables": "sudo iptables -vnL --line",
+    "selinux": "sudo getenforce",
+    "os_version": "cat /etc/issue",
+    "python_version": "python -V",
+    "RAM_stats": "cat /proc/meminfo",
+    "WD_list": "ls -l ."
 }
 
 
 def append_content_to_file(command_object, file_path='/tmp/cef_get_info'):
-    output = repr(command_object)
+    output = repr(command_object).replace('%', '%%')
     command_tokens = ["sudo", "bash", "-c", "printf '" + "\n" + output + "' >> " + file_path]
     try:
         write_new_content = subprocess.Popen(command_tokens, stdout=subprocess.PIPE)
-        time.sleep(2)
+        time.sleep(0.5)
         o, e = write_new_content.communicate()
     except Exception:
+        print(str(command_object.command) + "was not documented successfully")
+    if e is not None:
         print(str(command_object.command) + "was not documented successfully")
 
 
 def run_command(command):
-    command_to_run = subprocess.Popen(command_dict[command], stdout=subprocess.PIPE)
+    command_to_run = subprocess.Popen(command_dict[command].split(' '), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     try:
         o, e = command_to_run.communicate()
     except Exception:
